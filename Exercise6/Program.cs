277bc1c56066a2166beb11a12b4bd0d30a019f79
puglisac <alan.c.puglisi@gmail.com>
2020-11-19 15:58:14 -0600

    ready to submit

---
+++ Exercise6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an angle in degrees: ");
+            try
+            {
+                float angleDeg = float.Parse(Console.ReadLine());
+                float cosine = (float)Math.Cos((Math.PI / 180) * angleDeg);
+                float sine = (float)Math.Sin((Math.PI / 180) * angleDeg);
+                Console.WriteLine($"The cosine of {angleDeg} is {cosine}.");
+                Console.WriteLine($"The sine of {angleDeg} is {sine}.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
